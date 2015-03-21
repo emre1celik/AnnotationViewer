147d984c394a5741abc60acd64d653b2367a2550
dramenia <info@dramenia.nl>
2015-03-21 10:33:01 +0100

    added classes

---
+++ src/NL/HAN/PG2/Polynucleotide/Polynucleotide.java
@@ -0,0 +1,7 @@
+package NL.HAN.PG2.Polynucleotide;
+
+/**
+ * Created by christiaan on 21-3-2015.
+ */
+public class Polynucleotide {
+}
---
+++ src/NL/HAN/PG2/Protein/Protein.java
@@ -0,0 +1,9 @@
+package NL.HAN.PG2.Protein;
+
+/**
+ * Created by christiaan on 21-3-2015.
+ */
+public class Protein {
+    public int readingFrame;
+
+}
