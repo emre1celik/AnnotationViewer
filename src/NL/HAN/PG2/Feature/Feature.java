c38d79935374197464f67b79d21ecc7e2b2b644a
dramenia <info@dramenia.nl>
2015-03-21 10:43:28 +0100

    added classes

--- src/NL/HAN/PG2/Feature/Feature.java
+++ src/NL/HAN/PG2/Feature/Feature.java
@@ -4,4 +4,21 @@ package NL.HAN.PG2.Feature;
  * Created by christiaan on 21-3-2015.
  */
 public class Feature {
+    private String name;
+    private int begin;
+    private int end;
+    private byte color;
+
+    public void setFeature(){
+
+    }
+    public String getFeature(){
+        return name;
+    }
+    public int getFeature(String name){
+        return begin;
+    }
+    public byte getFeature(byte empty){
+        return color;
+    }
 }
