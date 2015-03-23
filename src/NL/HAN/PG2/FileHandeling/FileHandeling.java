794285805c4865a168d09e96e2ead684bd453ad0
dramenia <info@dramenia.nl>
2015-03-23 11:20:57 +0100

    FileHandeling template

---
+++ src/NL/HAN/PG2/FileHandeling/FileHandeling.java
@@ -0,0 +1,24 @@
+package NL.HAN.PG2.FileHandeling;
+
+/**
+ * Created by christiaan on 23-3-2015.
+ */
+public class FileHandeling {
+    private String name;
+    private String type;
+    private String adres;
+
+    public void setFile(String fileadres){
+
+    }
+    public String getType(){
+
+    }
+    public void openFile(){
+
+    }
+    public void saveFile(){
+        
+    }
+
+}
--- src/NL/HAN/PG2/Sequence/Sequence.java
+++ src/NL/HAN/PG2/Sequence/Sequence.java
@@ -1,5 +1,6 @@
 package NL.HAN.PG2.Sequence;
 
+
 /**
  * Created by christiaan on 21-3-2015.
  */
