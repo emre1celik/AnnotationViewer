f4ba8a7d234946954d17064c7db0cc2c11a2290d
heleen23 <heleen23@users.noreply.github.com>
2015-03-23 20:07:58 +0100

    Update Viewer
    Enorme lazy manier van een commit

--- src/NL/HAN/PG2/Viewer/Viewer.java
+++ src/NL/HAN/PG2/Viewer/Viewer.java
@@ -6,10 +6,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 /**
  * Created by christiaan on 11-3-2015.
+ * Edit Heleen 23/3/2015.
  */
 public class Viewer extends JFrame implements ActionListener {
 
@@ -22,10 +27,13 @@ public class Viewer extends JFrame implements ActionListener {
         setBounds(inset, inset,
                 screenSize.width - inset * 2,
                 screenSize.height - inset * 2);
+        int framewidth = screenSize.width - inset * 2;
+        int frameheight = screenSize.height/3;
+        System.out.println(frameheight);
         desktop = new JDesktopPane();
-        createFrame("Polynucleotide");
-        createFrame("Amino acids");
-        createFrame("Features");
+        createFrame("Polynucleotide",0,0,framewidth,frameheight);
+        createFrame("Amino acids",0,frameheight,framewidth,frameheight);
+        createFrame("Features",0,frameheight*2,framewidth,frameheight);
         setContentPane(desktop);
         setJMenuBar(createMenuBar());
 
@@ -44,6 +52,30 @@ public class Viewer extends JFrame implements ActionListener {
         menuItem.setActionCommand("new");
         menuItem.addActionListener(this);
         file.add(menuItem);
+        
+        menuItem = new JMenuItem("Open file");
+        //menuItem.setMnemonic(KeyEvent.VK_O);
+        menuItem.setActionCommand("openfile");
+        menuItem.addActionListener(this);
+        file.add(menuItem);
+
+        menuItem = new JMenuItem("Open record");
+        //menuItem.setMnemonic(KeyEvent.VK_O);
+        menuItem.setActionCommand("openrecord");
+        menuItem.addActionListener(this);
+        file.add(menuItem);
+
+        menuItem = new JMenuItem("Import/Blast");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menuItem.setActionCommand("blast");
+        menuItem.addActionListener(this);
+        file.add(menuItem);
+
+        menuItem = new JMenuItem("Print");
+        menuItem.setMnemonic(KeyEvent.VK_P);
+        menuItem.setActionCommand("print");
+        menuItem.addActionListener(this);
+        file.add(menuItem);
 
         menuItem = new JMenuItem("Quit");
         menuItem.setMnemonic(KeyEvent.VK_Q);
@@ -57,39 +89,98 @@ public class Viewer extends JFrame implements ActionListener {
 
         menuItem = new JMenuItem("ORF");
         menuItem.setMnemonic(KeyEvent.VK_O);
+        menuItem.setActionCommand("orfs");
+        menuItem.addActionListener(this);
         create.add(menuItem);
 
         JMenu help = new JMenu("Help");
         help.setMnemonic(KeyEvent.VK_H);
         menuBar.add(help);
 
+        menuItem = new JMenuItem("Read Help");
+        menuItem.setMnemonic(KeyEvent.VK_H);
+        menuItem.setActionCommand("help");
+        menuItem.addActionListener(this);
+        help.add(menuItem);
+
         menuItem = new JMenuItem("About");
         menuItem.setMnemonic(KeyEvent.VK_A);
+        menuItem.setActionCommand("about");
+        menuItem.addActionListener(this);
         help.add(menuItem);
 
         return menuBar;
     }
 
     public void actionPerformed(ActionEvent e) {
-        if ("new".equals(e.getActionCommand())) {
-            createFrame("Something");
-        } else {
-            quit();
+        String action = e.getActionCommand();
+        switch (action) {
+            case "new": {
+                createFrame("Something",0,0,0,0);
+                break;
+            }
+            case "help": {
+                break;
+            }
+            case "openfile": {
+                openFile();
+                break;
+            }
+            case "about": {
+                showAbout();
+                break;
+            }
+            default: {
+                quit();
+                break;
+            }
         }
     }
 
-    protected void createFrame(String header) {
+    private void createFrame(String header, int xpos, int ypos, int framewidth, int frameheight) {
         SubFrame frame = new SubFrame(header);
         frame.setVisible(true);
         desktop.add(frame);
         try {
             frame.setSelected(true);
+            frame.setBounds(xpos, ypos, framewidth, frameheight);
         } catch (java.beans.PropertyVetoException e) {}
     }
 
     protected void quit() {
         System.exit(0);
     }
+    
+    protected void showAbout() {
+        createFrame("About",200,200,200,200);
+    }
+    
+    protected void openFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        File selectedFile;
+        int reply;
+        reply = fileChooser.showOpenDialog(this);
+        if (reply == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fileChooser.getSelectedFile();
+            readFile(selectedFile);
+        }
+    }
+
+    public void readFile(File selectedFile) {
+        BufferedReader inFile;
+        StringBuilder sequentie = new StringBuilder();
+        try {
+            inFile = new BufferedReader(new FileReader(selectedFile));
+            String line;
+            while ((line = inFile.readLine()) != null) {
+                sequentie.append(line);
+            }
+            inFile.close();
+        }
+        catch (IOException e) {
+            JOptionPane.showMessageDialog(null,"File Error: " + e.toString());
+        }
+}
 
     public static void createAndShowGUI() {
         JFrame.setDefaultLookAndFeelDecorated(true);
